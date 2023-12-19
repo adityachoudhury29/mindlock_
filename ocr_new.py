96fd502ce1678d02d8696bddcaebb8cf083e2edf
adityachoudhury29 <achoudhury2004@gmail.com>
2023-12-19 11:15:19 +0530

    ocr_removed unused function

--- ocr_new.py
+++ ocr_new.py
@@ -40,12 +40,3 @@ def split_boxes(board):
             boxes.append(box)
     cv2.destroyAllWindows()
     return boxes
-
-def displayNumbers(img, numbers, color=(0, 255, 0)):
-    W = int(img.shape[1]/9)
-    H = int(img.shape[0]/9)
-    for i in range (9):
-        for j in range (9):
-            if numbers[(j*9)+i] !=0:
-                cv2.putText(img, str(numbers[(j*9)+i]), (i*W+int(W/2)-int((W/4)), int((j+0.7)*H)), cv2.FONT_HERSHEY_COMPLEX, 2, color, 2, cv2.LINE_AA)
-    return img
